98304a65437c1ce345f6208425527e1d34794497
dan-primo <danielly.primo@gmail.com>
2020-05-31 20:46:17 -0300

    update

---
+++ 1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World <3");
+
+            Console.WriteLine("Execução concluída! Por favor tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
