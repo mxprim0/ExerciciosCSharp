4bd55b539e720ae0a551d0982c2327e836eac55d
dan-primo <danielly.primo@gmail.com>
2020-06-02 13:46:20 -0300

    update projeto 5

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execução Projeto 5 - Caracteres e Textos");
+
+            //character
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Exercicios" + 2020;
+            string listaAssuntos = 
+@"- .Net Core +
+- Banco de Dados
+- Azure";
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(listaAssuntos);
+
+            Console.ReadLine();
+        }
+    }
+}
