3b0b59d7a3a8d1e078e387dcb774ae16f1acfab0
dan-primo <danielly.primo@gmail.com>
2020-06-03 23:18:40 -0300

    projeto 8

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execução Projeto 8 - Condicionais 2");
+
+            int idadeMaria = 15;
+            // int quantidadePessoas = 2;
+            // bool acompanhado = quantidadePessoas >= 2;
+            bool acompanhado = true;
+
+            if (idadeMaria >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                    Console.WriteLine("Não pode entrar.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
