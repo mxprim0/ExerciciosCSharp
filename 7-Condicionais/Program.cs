7edb8bb085c578c3dce7883410d0f460c8dc9e41
dan-primo <danielly.primo@gmail.com>
2020-06-03 22:56:08 -0300

    update projeto 7

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execução Projeto 7 - Condicionais");
+
+            int idadeMaria = 15;
+            int quantidadePessoas = 2;
+
+            if (idadeMaria >= 18)
+            {
+                Console.WriteLine("Possui mais de 18 anos, pode entrar.");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("Menor de idade, mas está acompanhado. Pode entrar");
+                }
+                else
+                {
+                    Console.WriteLine("Menor de idade, não pode entrar.");
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
