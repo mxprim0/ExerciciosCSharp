5d99a3689d33c20e4f0bc84d75861e6e120bd3e7
dan-primo <danielly.primo@gmail.com>
2020-06-03 22:31:59 -0300

    update projeto 6

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execução Projeto 6");
+
+            int idade = 10;
+            int idadeDanielly = idade;
+
+            idade = 30;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeDanielly);
+
+            Console.ReadLine();
+        }
+    }
+}
