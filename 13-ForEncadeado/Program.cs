df22916f7d0cd20e47887f911265a8eeacf20749
dan-primo <danielly.primo@gmail.com>
2020-06-04 19:48:11 -0300

    projeto 13

---
+++ 13-ForEncadeado/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execução Projeto 13 - For");
+
+            // Escrevendo asteriscos com Breack
+            for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+            // Forma diferente de fazer asteriscos
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
