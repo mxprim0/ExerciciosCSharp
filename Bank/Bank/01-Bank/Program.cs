91348fffb9f994292044d6f885d826492cc74eb1
dan-primo <danielly.primo@gmail.com>
2020-06-05 20:29:26 -0300

    Novo Projeto Bank

---
+++ Bank/Bank/01-Bank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ContaCorrente();
+
+            Console.ReadLine();
+        }
+    }
+}
