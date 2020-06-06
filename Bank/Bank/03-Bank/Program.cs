9446b9c5d20a3defd4b765102138b4008a947f00
dan-primo <danielly.primo@gmail.com>
2020-06-05 22:39:34 -0300

    projeto 03

---
+++ Bank/Bank/03-Bank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a palavra "new" cria uma variavel de referencia para o objeto
+            ContaCorrente contaDaMaria = new ContaCorrente();
+            contaDaMaria.titular = "Maria";
+            contaDaMaria.agencia = 863;
+            contaDaMaria.numero = 863452;
+
+            ContaCorrente contaDaMariaJose = new ContaCorrente();
+            contaDaMaria.titular = "Maria";
+            contaDaMaria.agencia = 863;
+            contaDaMaria.numero = 863452;
+
+            Console.WriteLine(contaDaMaria = contaDaMariaJose);
+
+            // contaDaMaria = contaDaMariaJose;
+            // Console.WriteLine(contaDaMaria == contaDaMariaJose);
+
+            Console.ReadLine();
+        }
+    }
+}
