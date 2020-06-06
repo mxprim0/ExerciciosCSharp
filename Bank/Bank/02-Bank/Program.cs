ee567f853c769423482ac18ece83a0e5dbb784d9
dan-primo <danielly.primo@gmail.com>
2020-06-05 22:12:33 -0300

    projeto 02

---
+++ Bank/Bank/02-Bank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Maria";
+
+            Console.WriteLine(conta.titular);
+            Console.WriteLine(conta.agencia);
+            Console.WriteLine(conta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
