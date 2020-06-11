b61094487399d7c4b28b931572d601e9a1efd996
dan-primo <danielly.primo@gmail.com>
2020-06-11 12:27:15 -0300

    projeto 5

--- Bank/Bank/05-Bank/Program.cs
+++ Bank/Bank/05-Bank/Program.cs
@@ -10,7 +10,30 @@ namespace _05_Bank
     {
         static void Main(string[] args)
         {
-            Cliente maria = new Cliente();
+            // Cliente maria = new Cliente();
+
+            // maria.nome = "Maria";
+            // maria.profissao = "Dev DotNet";
+            // maria.cpf = "123.456.789.11";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            // conta.titular = maria;
+            conta.titular = new Cliente();
+            conta.titular.nome = "Maria Costa";
+            conta.titular.cpf = "123.456.789.11";
+            conta.titular.profissao = "Dev DotNet";
+            
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            // Console.WriteLine(maria.nome);
+            Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+
+            Console.ReadLine();
         }
     }
 }
