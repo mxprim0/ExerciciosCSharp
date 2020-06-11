fe80fd10bf50a38814f455d1254ea98678fc3f69
dan-primo <danielly.primo@gmail.com>
2020-06-11 11:42:57 -0300

    projeto 5

---
+++ Bank/Bank/05-Bank/ContaCorrente .cs
@@ -0,0 +1,60 @@
+﻿// using _05_Bank;
+
+namespace _05_Bank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            return true;
+
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+
+        /* Exemplo do código acima sem a simplificação:::
+         * 
+         * public bool Transferir( double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+         */
+    }
+}
+ 
---
+++ Bank/Bank/05-Bank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente maria = new Cliente();
+        }
+    }
+}
