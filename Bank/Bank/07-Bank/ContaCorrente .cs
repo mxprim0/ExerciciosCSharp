a748b85e9b4144b5b0608b30f133de2a9169c053
dan-primo <danielly.primo@gmail.com>
2020-06-12 11:10:14 -0300

    importante construtores proj7

---
+++ Bank/Bank/07-Bank/ContaCorrente .cs
@@ -0,0 +1,90 @@
+﻿// using _07_Bank;
+
+namespace _07_Bank
+{
+    public class ContaCorrente
+    {
+        private Cliente _titular;
+
+        public Cliente Titular { get; set; }
+
+        public int Agencia { get; set }
+        public int Numero { get; set }
+        
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                value
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+                // value só funciona dentro do método set
+            }
+        }
+
+        // Construtor do Objeto ContaCorrente::::
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+        
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+
+        /* Exemplo do código acima sem a simplificação:::
+         * 
+         * public bool Transferir( double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+         */
+    }
+}
+ 
---
+++ Bank/Bank/07-Bank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
