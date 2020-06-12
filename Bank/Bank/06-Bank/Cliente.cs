4c06b79255b5a8588fcc739ddab47e3c269d5f5f
dan-primo <danielly.primo@gmail.com>
2020-06-12 10:20:56 -0300

    importante metodos get/set proj6

---
+++ Bank/Bank/06-Bank/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Bank
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Profissao { get; set; }
+        // usar atalho "prop" pra criar metodos
+    }
+}
---
+++ Bank/Bank/06-Bank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Danie";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine(conta.Titular.Nome);
+
+            Console.ReadLine();
+        }
+    }
+}
