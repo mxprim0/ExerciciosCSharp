49ce18b41a71991be66ffce596f9836fdbe239eb
dan-primo <danielly.primo@gmail.com>
2020-06-18 11:02:33 -0300

    c# parte 3: Entendendo Herenca e Interface

---
+++ DigitalBank/DigitalBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalBank.Funcionarios
+{
+    public class Funcionario
+    {
+        // 0 - funcionario
+        // 1 - diretor
+        // 2 - designer
+        // 3 - gerente de conta corrente
+        // 4 - ...
+        private int _tipo;
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        //public double ObterBonificacao() Quando encontramos "obter" trocamos pelo Get
+        {
+            if (_tipo == 1)
+                return Salario;
+
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ DigitalBank/DigitalBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using DigitalBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario joao = new Funcionario(1);
+
+            joao.Nome = "Joao";
+            joao.CPF = "123456789-88";
+            joao.Salario = 2000;
+
+            Console.WriteLine(joao.Nome);
+            Console.WriteLine(joao.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
