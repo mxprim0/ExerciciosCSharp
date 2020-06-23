067bfe98d6fe4a6a5cf762c79cdc98ea98f64b6f
dan-primo <danielly.primo@gmail.com>
2020-06-22 21:37:11 -0300

    API AsP.NET | Pags Razor | Introducao | Add Controler | Add Views

---
+++ Asp.NetCore/MvcMovie/MvcMovie/Controllers/HelloWorldController .cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /*
+          public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+        
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        */
+    }
+}
