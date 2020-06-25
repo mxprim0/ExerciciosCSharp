ebc8f58e8d77b530fa21e1ba7ca6846a02b18826
dan-primo <danielly.primo@gmail.com>
2020-06-24 21:43:21 -0300

    Parte4 - Aplicar scaffold a páginas de filme

--- Asp.NetCore/MvcMovie/MvcMovie/Startup.cs
+++ Asp.NetCore/MvcMovie/MvcMovie/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie
 {
@@ -24,6 +26,9 @@ namespace MvcMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
